b2a417f1fffe1c282c15405787357729e500fa9d
Erofey Bashunov <cannor147@gmail.com>
2022-01-07 22:51:41 +0300

    feat(config): set up project structure

---
+++ hw3/src/HW3/Base.hs
@@ -0,0 +1,11 @@
+module HW3.Base
+  ( HiFun(..)
+  , HiValue(..)
+  , HiExpr(..)
+  , HiError(..)
+  ) where
+
+data HiFun = HiFun
+data HiValue = HiValue
+data HiExpr = HiExpr
+data HiError = HiError
